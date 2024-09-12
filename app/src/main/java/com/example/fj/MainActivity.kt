36fa137db2e32423f0c2a4eee05046fe9afc78f4
DavidRangel2405 <21151065@aguascalientes.tecnm.mx>
2024-09-12 07:21:42 -0600

    Add routing to app, .created Home and Menu

--- app/src/main/java/com/example/fj/MainActivity.kt
+++ app/src/main/java/com/example/fj/MainActivity.kt
@@ -282,7 +282,6 @@ fun Content2() {
             )
             Row(
                 modifier = Modifier
-                    .background(Color.Red)
                     .fillMaxWidth()
                     .padding(0.dp, 0.dp),
                 horizontalArrangement = Arrangement.Center
